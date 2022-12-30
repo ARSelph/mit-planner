69e4b363b1759bc208a405d305f80090affe2c15
Adam Selph <adam@selph.net>
2022-12-30 08:20:07 -0800

    remove public path from webpack

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
   },
   output: {
     filename: 'bundle.js',
-    publicPath: '/dist/',
+    // publicPath: '/dist/',
     path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
